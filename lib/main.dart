441c15461211c1aefd1ee9f82f5c255def7f7b76
Dowon Oh <dhehdnjs03@gmail.com>
2022-11-05 02:07:31 +0900

    Custom Dialog and set state with parent state

--- lib/main.dart
+++ lib/main.dart
@@ -13,67 +13,95 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var name = ["홍길동", "김길동", "이길동", "박길동", "최길동"];
+  var phoneNumbers = List<String>.generate(100, (index) => "unknown phone number");
+  var currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          floatingActionButton: Builder(
-            builder: (context) {
-              return FloatingActionButton(
-                onPressed: () {
-                  showDialog(context: context, builder: (context) {
-                    print(context.findAncestorWidgetOfExactType<MaterialApp>());
-                    return Dialog(child: Text("Hello"));
-                  });
-                },
-                child: Icon(Icons.smart_button),
-              );
-            }
-          ),
           appBar: AppBar(),
           body: ListView.builder(
             itemCount: 100,
             itemBuilder: (context, index) {
-              return ListTile(
-                leading: Icon(Icons.person_pin, size: 50),
-                title: Text(name[index % name.length]),
-                subtitle: Text('Description'),
+              return ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                ),
+                onPressed: () {
+                  setState(() {
+                    currentIndex = index;
+                  });
+                  showDialog(context: context, builder: (context) {
+                    return ContactModal();
+                  });
+                },
+                child: ListTile(
+                  leading: Icon(Icons.person_pin, size: 50),
+                  title: Text(name[index % name.length]),
+                  subtitle: Text(phoneNumbers[index]),
+                ),
               );
             },
           ),
-          bottomNavigationBar: BottomMenu(),
         ),
     );
   }
 }
 
-class BottomMenu extends StatelessWidget {
-  const BottomMenu({Key? key}) : super(key: key);
+class ContactModal extends StatefulWidget {
+  const ContactModal({Key? key}) : super(key: key);
+
+  @override
+  State<ContactModal> createState() => _ContactModalState();
+}
+
+class _ContactModalState extends State<ContactModal> {
+  var phoneNumber = "010-0000-0000";
 
   @override
   Widget build(BuildContext context) {
-    return BottomAppBar(
-      child: SizedBox(
-        height: 70,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    _MyAppState? myApp = context.findAncestorStateOfType<_MyAppState>();
+    return SizedBox(
+      width: 100,
+      height: 100,
+      child: Dialog(
+        child: Column(
           children: [
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.phone),
+            Text("${myApp?.name[myApp.currentIndex % myApp.name.length]}"),
+            TextField(
+              onChanged: (value) {
+                setState(() {
+                  phoneNumber = value;
+                });
+              },
+              decoration: InputDecoration(
+                hintText: "input phone number",
+              ),
             ),
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.message),
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.contact_page),
+            Row(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text("Cancel"),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    myApp?.setState(() {
+                      myApp.phoneNumbers[myApp.currentIndex] = phoneNumber;
+                    });
+                    Navigator.of(context).pop();
+                  },
+                  child: Text("Save"),
+                ),
+              ],
             ),
           ],
-        ),
+        )
       ),
     );
   }
 }
+
